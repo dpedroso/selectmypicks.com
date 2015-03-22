dc603f8533a5d2a558dd136ffdcd7178cfded97f
dpedroso <dp2@cox.net>
2015-03-22 14:34:45 -0400

    4

--- webapi/index.php
+++ webapi/index.php
@@ -17,20 +17,23 @@
 	}
 
 	if (!mysql_select_db($dbname)) {
-		echo "Unable to select mydbname: " . mysql_error();
+		echo "Unable to select dbname: " . mysql_error();
 		exit;
 	}
 
+/*********Passing URL Stored Procedure *********/
 	
 	// Extract calls and args from URL
-	$sp_name_args =  explode("?",$_SERVER[REQUEST_URI]);
-	
+	$pos = strpos($_SERVER[REQUEST_URI], "?")+1;
+	$sp_name_args =  substr($_SERVER[REQUEST_URI],$pos);
+
+	$delimeter = "|";
 	// Separate call and args
-	$sp_name_args = explode("&",$sp_name_args[1]);
+	$sp_name_args = explode($delimeter,$sp_name_args);
 	
 	// Assign stored procedure name
 	$sp_name = $sp_name_args[0];
-	
+
 	// Build out argument array
 	for($c=1;$c<sizeof($sp_name_args);$c++)
 		$args[] = $sp_name_args[$c];
@@ -38,7 +41,7 @@
 	// Call stored procedure from url
 	call($sp_name, $args);
 
-	
+/********* END Passing URL Stored Procedure *********/
 	
 	// Close connection
 	mysql_close($conn);
--- webapi/stored_procedure_calls.php
+++ webapi/stored_procedure_calls.php
@@ -1,10 +1,42 @@
 <?php
 
+	// Inserts
+	// webapi/?insert_new_user|name|email
+	
+	// Updates
+	// webapi/?update_user_by_id|id|email
+	
+	// Selects
+	// webapi/?select_user_by_id|id
+	// webapi/?select_all_users
+
     // Used to call stored procedures
 	function call($name, $args){
+
+		$type = explode("_",$name);
 	
-		$data = mysql_query("call ".$name."(".get_arg_values($args).")");
-		
+ 		switch($type[0]){
+			case "insert":
+				mysql_query("call ".$name."(".get_arg_values($args).")");
+				$last_insert_id = mysql_fetch_row(mysql_query("select last_insert_id()"));
+				echo $last_insert_id[0];
+				break;			
+			case "select":
+				$data = mysql_query("call ".$name."(".get_arg_values($args).")");
+				select_data($data);
+				break;			
+			case "update":
+				mysql_query("call ".$name."(".get_arg_values($args).")");
+				break;
+			default:
+				return false;
+			break;
+		} 
+		//echo mysql_error();
+	}
+	
+	function select_data($data){
+	
 		if($data==true){
 		
 			// Build an array of data
@@ -15,19 +47,21 @@
 			// Output JSON string
 			echo json_encode($data_array);
 		}
-
+	
 	}
 	
 	function get_arg_values($args){
 	
+		// no args = return
 		if(sizeOf($args)==0)
 			return "";
 			
 		$arg_str = "";
 		foreach($args as $arg){
-			$k_v = explode("=",$arg);
-			$arg_str.=$k_v[1].",";
+			// ex. 'John Smith',
+			$arg_str.="'".urldecode($arg)."'".",";
 		}
+		// Trim last comma from argument string
 		return rtrim($arg_str, ",");
 	}
 	
