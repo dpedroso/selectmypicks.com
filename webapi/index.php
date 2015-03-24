6696eb9b5782784539415b380a37aa51fe40f0e0
dpedroso <dp2@cox.net>
2015-03-23 21:13:50 -0400

    Added post

--- webapi/index.php
+++ webapi/index.php
@@ -2,6 +2,7 @@
 	header("Access-Control-Allow-Origin: *");
 	include("stored_procedure_calls.php");
 
+	
 	// Setup database access
 	$servername = "localhost";
 	$username = "pickuplo_test";
@@ -23,21 +24,14 @@
 
 /*********Passing URL Stored Procedure *********/
 	
-	// Extract calls and args from URL
+	// Extract call from URL
 	$pos = strpos($_SERVER[REQUEST_URI], "?")+1;
-	$sp_name_args =  substr($_SERVER[REQUEST_URI],$pos);
-
-	$delimeter = "|";
-	// Separate call and args
-	$sp_name_args = explode($delimeter,$sp_name_args);
+	$sp_name =  substr($_SERVER[REQUEST_URI],$pos);
 	
-	// Assign stored procedure name
-	$sp_name = $sp_name_args[0];
+	$args = array();
+	foreach($_POST as $key => $value)
+		$args[$key]=$value;
 
-	// Build out argument array
-	for($c=1;$c<sizeof($sp_name_args);$c++)
-		$args[] = $sp_name_args[$c];
-	
 	// Call stored procedure from url
 	call($sp_name, $args);
 
